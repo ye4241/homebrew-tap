0bdf90471440feb4f7d1e8ee7c8d28c955cff326
ye4241 <6803102+ye4241@users.noreply.github.com>
2025-10-09 14:52:35 +0800

    Fix DotnetReactor formula;

--- Formula/dotnet-reactor.rb
+++ Formula/dotnet-reactor.rb
@@ -4,29 +4,26 @@ class DotnetReactor < Formula
   url "https://www.eziriz.com/download.php?download=dotnet_reactor_7_3_0_0_macos-x64.zip"
   version "7.3.0.0"
   sha256 "079d8c87b3ab6887215492dcab7fdd6529a21b63b8fcb9a0fd94079ccba3c87b"
-  
+
   def install
     libexec.install Dir["*"]
-    
     chmod "+x", libexec/"dotNET_Reactor"
     chmod "+x", libexec/"dotNET_Reactor.Console"
-    
     system "xattr", "-cr", libexec
-    
     bin.install_symlink libexec/"dotNET_Reactor"
     bin.install_symlink libexec/"dotNET_Reactor.Console"
   end
-  
-  test do
-    assert_predicate bin/"dotNET_Reactor", :exist?
-    assert_predicate bin/"dotNET_Reactor.Console", :exist?
-  end
-  
+
   def caveats
     <<~EOS
       dotNET Reactor has been installed with two commands:
         - dotNET_Reactor          (GUI version)
         - dotNET_Reactor.Console  (Console version)
     EOS
+  end
+
+  test do
+    assert_path_exists bin/"dotNET_Reactor"
+    assert_path_exists bin/"dotNET_Reactor.Console"
   end
 end
